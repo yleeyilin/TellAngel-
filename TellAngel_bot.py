a96ebc9487190c196a9b4d920c0318b366001d31
yleeyilin <116061001+yleeyilin@users.noreply.github.com>
2023-01-15 11:15:32 +0800

    Update TellAngel_bot.py

--- TellAngel_bot.py
+++ TellAngel_bot.py
@@ -5,7 +5,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from textblob import TextBlob
 positiveArr = ["No act of kindness, no matter how small, is ever wasted. Keep it up!","It takes courage to be kind. Well done!","Let others see the good that you see in them. You're doing great!","Kindness is free - sprinkle that stuff everywhere","Love you kind message. If you were a booger, I will pick you :)","You cannot do kindness too soon, for you never know how soon it will be too late.-Ralph Waldo Emerson","Kindness is not what you do, but who you are.","Kindness is something anyone can give without losing anything themselves.","Kindness is seeing the best in others when they cannot see it in themselves." ,"When you are kind to others, it not only changes you, it not only changes you, it changes the world. - Harold Kushner","How do we change the world? One random act of kindess at a time - Morgan Freeman","How far that little candle throws his beams! So shines a good deed in a weary world. - William Shakespeare","Know that your kindness has a ripple effect in the universe! :)", "Spread love everywhere you go!", "to kindness and love, the things we need most!", "keep it up! treat others the way you want to be treated"]
 negativeArr = ["LOL someone really thinks they are the main character…Reconsider what you just said.","God help… You might want to think about what you said there","Even the president does not have the right to be this rude! Think again…","Please check yourself before you wreck yourself.","For someone with your IQ level, you sure are brave…Think about what you just said.","Goodness, someone woke up on the wrong side of the bed! Consider rephrasing.","Ok boomer. Are you sure you want to be so rude","Say what you mean, but dont say it mean","I am so sorry you are having a bad day, but try to be nicer","Meanness is a sign of weakness! reevaluate your last message.","Any fool can criticize, condemn and complain and most fools do. - Benjamin Franklin","Thats not very nice. Consider rephrasing your message.", "Are you sure you should have said that? Think again!", "Won't that hurt their feelings? reconsider.", "Would you like to be spoken to that way? think again!"]
-updater = Updater(secrets.TOKEN, use_context = True)
+updater = Updater(${{ secrets.TOKEN }}, use_context = True)
 dispatcher= updater.dispatcher
 def start(update, context):
     update.message.reply_text("Hey, I am TellAngel, here to remind you to send kind messages to your precious friends. You are given a kindness score of 0. ")
@@ -32,7 +32,7 @@ def sentiment(update, context):
     else:
         score = 0
     blob = TextBlob(message).correct()
-    bot = telegram.Bot(token = secrets.TOKEN)
+    bot = telegram.Bot(token = ${{ secrets.TOKEN }})
     if blob.sentiment.polarity > 0:
         position = random.randint(0,len(positiveArr)-1)
         score += 1
